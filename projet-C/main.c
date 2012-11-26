479daa3d11961af32ba576909259b9b53932e249
mohamed <oulmaki@gmail.com>
2012-11-26 10:10:44 +0100

    etape1.1

--- projet-C/main.c
+++ projet-C/main.c
@@ -7,7 +7,7 @@
 //
 
 #include <stdio.h>
-char codage[52]= {'2p','3p','4p', '5p', '6p', '7p','8p', '9p', 'Ap', 'Bp','Cp','Dp', 'Ep', '2q','3q','4q','5q','6q','7q','8q', '9q', 'Aq','Bq','Cq','Dq','Eq','2k','3k','4k','5k','6k','7k','8k','9k','Ak','Bk','Ck','Dk','Ek','2t','3t','4t','5t','6t','7t','8t','9t','At','Bt','Ct','Dt','Et'};
+char* codage[52]= {"2p","3p","4p", "5p", "6p", "7p","8p", "9p", "Ap", "Bp","Cp","Dp", "Ep", "2q","3q","4q","5q","6q","7q","8q", "9q","Aq","Bq","Cq","Dq","Eq","2k","3k","4k","5k","6k","7k","8k","9k","Ak","Bk","Ck","Dk","Ek","2t","3t","4t","5t","6t","7t","8t","9t","At","Bt","Ct","Dt","Et"};
 
 
 int main(int argc, const char * argv[])
