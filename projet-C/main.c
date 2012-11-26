21843a7a9c021cf17f29f717839f463d50ba5c0c
mohamed <oulmaki@gmail.com>
2012-11-26 09:25:08 +0100

    Initial Commit

---
+++ projet-C/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  projet-C
+//
+//  Created by Mohamed on 26/11/12.
+//  Copyright (c) 2012 Mohamed. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
